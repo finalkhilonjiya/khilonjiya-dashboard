c9533c2f4464fa2cb047fabf43dc41340d0d063a
finalkhilonjiya <finalkhilonjiya@gmail.com>
2026-05-12 20:44:26 +0530

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,5 +1,6 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { createBrowserClient, createServerClient } from "@supabase/ssr"
+import { createClient as createSupabaseClient } from "@supabase/supabase-js"
+import { cookies } from "next/headers"
 
 export async function createClient() {
   const cookieStore = await cookies()
@@ -12,41 +13,35 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
+
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
+              cookieStore.set(name, value, options)
             )
-          } catch {
-            // The "setAll" method was called from a Server Component.
-          }
+          } catch {}
         },
       },
-    },
+    }
   )
 }
 
-export async function createAdminClient() {
-  const cookieStore = await cookies()
+export function createBrowserSupabaseClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
 
-  return createServerClient(
+export function createAdminClient() {
+  return createSupabaseClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll()
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
-            )
-          } catch {
-            // The "setAll" method was called from a Server Component.
-          }
-        },
+      auth: {
+        persistSession: false,
+        autoRefreshToken: false,
       },
-    },
+    }
   )
 }
