d89f0f3c61fd1edcdb35d279c9ca805b9c9a25d5
finalkhilonjiya <finalkhilonjiya@gmail.com>
2026-05-13 13:59:32 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import tailwindcssAnimate from 'tailwindcss-animate'
 
 const config: Config = {
   darkMode: ['class'],
@@ -8,6 +9,7 @@ const config: Config = {
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
+
   theme: {
     extend: {
       colors: {
@@ -73,7 +75,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+
+  plugins: [tailwindcssAnimate],
 }
 
 export default config
